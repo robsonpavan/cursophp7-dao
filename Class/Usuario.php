6ebc5d242c3a2e6661f189d60f4afdf051366d93
Robson Alves Pavan <robsonpavan@yahoo.com.br>
2019-09-02 11:09:24 -0300

    Implementada opções de consulta ao BD na classe Usuario

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -40,13 +40,16 @@ class Usuario{
         $this->dtcadastro = $dtcadastro;
     }
 
+    //Função para realizar um select no BD buscando um usuário por ID
     public function loadById($id){
         $sql = new Sql();
         
+        //Chamando metodo "select" da classe SQL passando os paramêtros: comando sql e array para preparação do comando sql
         $result = $sql->select("SELECT * FROM tb_usuario WHERE idUsuario = :ID", array(
             ":ID" => $id
         ));
         
+        //Testando se a contulta não retornou vazio e atribuindo o resultado ao objeto usuário
         if (isset($result[0])){
             $row = $result[0];
             
@@ -59,7 +62,9 @@ class Usuario{
         
     }
     
+    //Metodo para permitir a escrita do objeto
     public function __toString() {
+        //Configurando a escrita do objeto usuário no formato json
         return json_encode(array(
             "idUsuario" => $this->getIdusuario(),
             "desLogin" => $this->getDeslogin(),
@@ -67,6 +72,54 @@ class Usuario{
             "dtCadastro" => $this->getDtcadastro()->format("d/m/Y H:i:s")
         ));
     }
+    
+    //Metodo para buscar todos os usuários do BD
+    public function getList(){
+        
+        $sql = new Sql();
+        
+        return $sql->select("SELECT * FROM tb_usuario ORDER BY deslogin;");
+        
+    }
+    
+    //Meotodo para buscar usuários pelo Login ou opor parte do login
+    public function search($login){
+        
+        $sql = new Sql();
+        
+        //Utilizando comando SQL com LIKE para buscar qualquer registro que contenha no login o conteúdo infomeado pela variável login
+        return $sql->select("SELECT * FROM tb_usuario WHERE desLogin LIKE :SEARCH ORDER BY desLogin", array(
+            ':SEARCH'=>"%".$login."%"
+        ));
+    }
+    
+    //Metodo para retornar as informações do usuário após validar seu login e sua senha
+    public function login($login, $password){
+        
+         $sql = new Sql();
+        
+        //Chamando metodo "select" da classe SQL passando os paramêtros: comando sql e array para preparação do comando sql
+        $result = $sql->select("SELECT * FROM tb_usuario WHERE desLogin = :LOGIN AND desSenha = :PASSWORD", array(
+            ":LOGIN" => $login,
+            ":PASSWORD" => $password
+        ));
+        
+        //Testando se a contulta não retornou vazio e atribuindo o resultado ao objeto usuário
+        if (isset($result[0])){
+            $row = $result[0];
+            
+            $this->setIdusuario($row['idUsuario']);
+            $this->setDeslogin($row['desLogin']);
+            $this->setDessenha($row['desSenha']);
+            $this->setDtcadastro(new DateTime($row['dtCadastro']));
+        
+        } else{
+            throw new Exception("Login e/ou senha inválidos");
+        }
+        
+        
+    }
+    
     
 }
 
--- index.php
+++ index.php
@@ -3,11 +3,23 @@
 
 require_once ("config.php");
 
-$sql = new Sql();
 
+//Carregs 1 usuário
+//$usuario = new Usuario();
+//$usuario->loadById('2');
+//echo $usuario;
+
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Consultar dados do usuário confirmando login e senha
 $usuario = new Usuario();
-
-$usuario->loadById('2');
+$usuario->login("jose", "senha12");
 
 echo $usuario;
 
